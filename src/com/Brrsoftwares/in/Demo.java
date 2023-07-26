f0a88b110c53c73fa75fdd2cfcbadfe07dc1cd18
koushik <koushik.anchuri2001@gmail.com>
2023-07-26 11:18:39 +0530

    initial commit

---
+++ src/com/Brrsoftwares/in/Demo.java
@@ -0,0 +1,31 @@
+package com.Brrsoftwares.in;
+
+import java.util.Arrays;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		
+		int []arr = {2,5,4,3,5,6,54,34,3};
+		
+		int temp;
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]>arr[j]) {
+					temp=arr[i];
+					
+				arr[i]=arr[j];
+				arr[j]=temp;
+				
+					
+				}
+			
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+		
+		
+	}
+
+}
